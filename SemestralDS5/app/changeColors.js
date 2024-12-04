c4b931478b3ea201b7f33dd8a3ee2bb91f39a885
brian <conex0770@gmail.com>
2024-12-04 13:38:54 -0500

    cambio GPPD

---
+++ SemestralDS5/app/changeColors.js
@@ -0,0 +1,18 @@
+
+const colorsChanges = (() => {
+
+    const htmlElements = {
+        select: document.querySelector("#api-method"),
+    } 
+
+    const funcChange = () => {
+        const selectedOption = htmlElements.select.options[htmlElements.select.selectedIndex];
+        // Aplicar el color del fondo según la clase del <option> seleccionado
+        htmlElements.select.style.backgroundColor = window.getComputedStyle(selectedOption).backgroundColor;
+        htmlElements.select.style.color = window.getComputedStyle(selectedOption).color;
+        htmlElements.select.style.fontWeight = window.getComputedStyle(selectedOption).fontWeight;
+    }
+
+    htmlElements.select.addEventListener("change", funcChange)
+    
+})();
